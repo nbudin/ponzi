68cd93515f40787847e52eca65fa01523f8e79e7
Nat Budin <natbudin@gmail.com>
2007-09-29 22:59:46 +0000

    configure smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,3 +16,9 @@ config.action_controller.perform_caching             = true
 
 # Disable delivery errors if you bad email addresses should just be ignored
 # config.action_mailer.raise_delivery_errors = false
+
+ActionMailer::Base.smtp_settings = {
+  :address  => "ponzi.natbudin.com",
+  :port  => 25, 
+  :domain  => 'ponzi.natbudin.com'
+    }

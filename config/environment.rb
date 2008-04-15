eb31db256e094bed34aed805d45e83166dc83736
Nat Budin <natbudin@gmail.com>
2008-04-15 16:03:38 +0000

    Freeze engines to Rails 2.0 branch

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,10 @@
 # Be sure to restart your web server when you modify this file.
 
+RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
+
 # Uncomment below to force Rails into production mode when 
 # you don't control web/app server and can't set it the proper way
-# ENV['RAILS_ENV'] ||= 'production'
-
-# Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.0.2'
+ENV['RAILS_ENV'] ||= 'production'
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')

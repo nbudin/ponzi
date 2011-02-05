cc589cebf7aaf4327fd73e60cfba7573e7b7bfef
Nat Budin <natbudin@gmail.com>
2011-02-05 08:16:37 -0500

    Change secret; use one from env if it exists

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = '9c6bdf90ef0b4a987c1e24f8ffb78b22e65b5168d5cf97e2148b6dd8ecbf99af6e6ac5816f2e74a18fd860695805a2eec7b557478c7337bfb8c8f2690dbba86e'
+Rails.application.config.secret_token = ENV['SECRET_TOKEN'] || '9c6bdf90ef0b4a987c1e24f8ffb78b22e65b5168d5cf97e2148b6dd8ecbf99af6e6ac5816f2e74a18fd860695805a2eec7b557478c7337bfb8c8f2690dbba864'
